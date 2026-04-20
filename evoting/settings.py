214f5cf6392dd4c6584bc0855370375dc8556766
Anilm303 <anilmr1515@gmail.com>
2026-04-20 16:51:17 +0545

    first commit

--- evoting/settings.py
+++ evoting/settings.py
@@ -88,10 +88,11 @@ if DATABASE_URL:
         "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=not DEBUG)
     }
 else:
+    sqlite_name = "/tmp/db.sqlite3" if os.getenv("VERCEL") else BASE_DIR / "db.sqlite3"
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": sqlite_name,
         }
     }
 
