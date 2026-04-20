e14da9cc02bd86e0cf376e82ca366d08a495c677
Anilm303 <anilmr1515@gmail.com>
2026-04-20 17:05:25 +0545

    first commit

--- api/index.py
+++ api/index.py
@@ -4,12 +4,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evoting.settings")
 
 import django
 from django.core.management import call_command
-from django.core.wsgi import get_wsgi_application
 
 
 if not os.getenv("DATABASE_URL"):
 	django.setup()
-	call_command("migrate", interactive=False, run_syncdb=True, verbosity=0)
+	call_command("migrate", interactive=False, verbosity=0)
+
+from django.core.wsgi import get_wsgi_application
 
 # Vercel Python runtime expects a WSGI app callable.
 app = get_wsgi_application()
