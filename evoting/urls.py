cd7bdbf083c56197e8fe1e628933f71b2d497e1e
Anilm303 <anilmr1515@gmail.com>
2026-04-21 10:36:17 +0545

    ..

--- evoting/urls.py
+++ evoting/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.templatetags.static import static
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=static("voting/favicon.svg"), permanent=False)),
     path("admin/", admin.site.urls),
     path("", include("voting.urls")),
 ]
